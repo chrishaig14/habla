4b2c3e87946fe33ae51c951af565635ac0cf6a47
chris <you@example.com>
2018-05-05 18:23:06 -0300

    g_k.m que faltaba actualizar

--- tp6/g_k.m
+++ tp6/g_k.m
@@ -1,5 +1,9 @@
-function [ g ] = g_k( x,y, sigma_k, u_k, pi_k )
-    x = [x,y];
-    g = -1/2*log(det(sigma_k))-1/2*(x-u_k)*inv(sigma_k)*(x-u_k).'+log(pi_k);
-end
+% function [ g ] = g_k( x,y, sigma_k, u_k, pi_k )
+%     x = [x,y];
+%     g = -1/2*log(det(sigma_k))-1/2*(x-u_k)*inv(sigma_k)*(x-u_k).'+log(pi_k);
+% end
 
+function [ g ] = g_k( x,y, det_sigma_k, inv_sigma_k, u_k, pi_k )
+    x = [x,y];
+    g = -1/2*log(det_sigma_k)-1/2*(x-u_k)*inv_sigma_k*(x-u_k).'+log(pi_k);
+end
