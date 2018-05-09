d7265016f3bc27cd25c895d0d47493d63f98f3e9
chris <you@example.com>
2018-05-09 16:58:41 -0300

    Acomodo nombres de los archivos

--- tp6/lda.m
+++ tp6/lda.m
@@ -2,214 +2,67 @@ as = load('a.txt');
 os = load('o.txt');
 us = load('u.txt');
 
+c = {as, os, us};
+
+legends = ['a';'o';'u'];
+colors = {[1,0,0],[0,1,0],[0,0,1]};
+
+f1_min = 0;
+f1_max = 1500;
+f2_min = 0;
+f2_max = 1800;
+
+
+%% K : cantidad de clases
+
+K = length(c);
+
 %% mezclar
 
-as = shuffle(as);
-os = shuffle(os);
-us = shuffle(us);
+for i = 1:K
+    c{i} = shuffle(c{i});
+end
 
 %% me quedo con 40 muestras y sólo los 2 primeros formantes
 
+f = cell(1, K); % train
+t = cell(1,K); % test
+
+n_f = 0; % # train
+n_t = 0;% # test
+
 N = 40;
 
-f_a = as(1:40,1:2);
-f_o = os(1:40,1:2);
-f_u = us(1:40,1:2);
+for k = 1:K
+    f{k} = c{k}(1:N, 1:2);
+    n_f = n_f + length(f{k});
+    t{k} = c{k}(N+1:end,1:2);
+    n_t = n_t + length(t{k});
+end
 
 %% graficar primeros dos formantes
 
 figure;
-
-plot(f_a(:,1),f_a(:,2),'ro');
-hold on;
-
-plot(f_o(:,1),f_o(:,2),'go');
-hold on;
-
-plot(f_u(:,1),f_u(:,2),'bo');
-hold on;
-
-axis equal;
-
-xlabel('F1 [Hz]');
-ylabel('F2 [Hz]');
-
-legend('a','o','u');
-
-%% calcular u y sigma
-
-u_a = mean(f_a);
-u_o = mean(f_o);
-u_u = mean(f_u);
-
-sigma_a = calcular_sigma(f_a,u_a);
-sigma_o = calcular_sigma(f_o,u_o);
-sigma_u = calcular_sigma(f_u,u_u);
-
-p_a = 1/3;
-p_o = 1/3;
-p_u = 1/3;
-
-sigma = p_a*sigma_a + p_o*sigma_o + p_u*sigma_u;
-
-%% clasificador
-
-x_a = as(41:50,1:2);
-x_o = os(41:50,1:2);
-x_u = us(41:50,1:2);
-
-xs = [x_a;x_o;x_u];
-ws = [ones(1,10), ones(1,10)*2, ones(1,10)*3];
-
-c = zeros(1,30);
-
-for i = 1: length(xs)
-    x = xs(i,:);
-    % p(a|x)
-    p_a_x = g_k(x(1),x(2),sigma, u_a, p_a);
-    % p(o|x)
-    p_o_x = g_k(x(1),x(2),sigma, u_o, p_o);
-    % p(u|x)
-    p_u_x = g_k(x(1),x(2),sigma, u_u, p_u);
-    
-    [m,l] = max([p_a_x,p_o_x,p_u_x]);
-    
-    c(i) = l;
-end
-
-x_as = xs(c==1,:);
-x_os = xs(c==2,:);
-x_us = xs(c==3,:);
-colors = ['r*','g*','b*'];
-
-figure;
-plot(x_as(:,1),x_as(:,2),'ro');
-hold on;
-plot(x_os(:,1),x_os(:,2),'go');
-hold on;
-plot(x_us(:,1),x_us(:,2),'bo');
-hold on;
-
-legend('a','o','u');
-
-
-%% calcular error
-fprintf('Error: %0.2f %% \n', sum(ws ~= c)/30*100);
-
-f1 = 0:25:2000;
-f2 = 0:25:2000;
-[F1,F2] = meshgrid(f1,f2);
-
-u = zeros(3,2);
-K = 3;
-xk = cell(1,K);
-
-u(1,:) = u_a;
-u(2,:) = u_o;
-u(3,:) = u_u;
-
-xk{1} = x_as;
-xk{2} = x_os;
-xk{3} = x_us;
-
-z = cell(1,K);
-
 for k=1:K
-
-    u_k = u(k,:);
-%     sigma_k = calcular_sigma(xk{k},u_k);
-    pi_k = 1/3;
-    z{k} = zeros(size(F1));
-    for i=1:numel(F1)
-        x = F1(i);
-        y = F2(i);
-        z{k}(i) = g_k(x,y,sigma,u_k, pi_k);
-    end
-
-    figure;
-    size(z{k})
-    surf(F1,F2,z{k},'EdgeColor','none');
-    xlabel('x');
-    ylabel('y');
-	zlabel('z');
-    view(2)
+    plot(f{k}(:,1),f{k}(:,2),'o','color',colors{k});
+    hold on;
+end
+legend(legends);
+    xlim([f1_min, f1_max])
+    ylim([f2_min, f2_max])
     
-end
+title('Datos entrenamiento');
 
-clas = zeros(size(F1));
-
-
-
-for i=1:numel(F1)
-        v = zeros(1,K);
-        
-        for k=1:K
-            v(k) = z{k}(i);
-        end
-        
-        [m,clas(i)] = max(v);
-end
-
-    figure;
-    h = surf(F1,F2,clas,'EdgeColor','none');
-    xlabel('x');
-    ylabel('y');
-	zlabel('z');
-    view(2)
-
-
-
-
-
-
-
-
-
-% %% graficar elipses de varianza
-% N = 50,
-% t = [0:2*pi/(N-1):2*pi];
-% y1 = sin(t);
-% y2 = cos(t);
-% 
-% y = [y1;y2];
-% 
-% e_a = chol(sigma_a);
-% xa = e_a'*y+u_a.';
-% 
-% e_o = chol(sigma_o);
-% xo = e_o'*y+u_o.';
-% 
-% e_u = chol(sigma_u);
-% xu = e_u'*y+u_u.';
-% 
 % figure;
-% plot(xa(1,:),xa(2,:));
-% hold on;
-% plot(xo(1,:),xo(2,:));
-% hold on;
-% plot(xu(1,:),xu(2,:));
-% hold on;
-% size(y)
-% 
-% %figure;
 % 
 % plot(f_a(:,1),f_a(:,2),'ro');
 % hold on;
 % 
-% plot(f_o(:,1),f_o(:,2),'bo');
+% plot(f_o(:,1),f_o(:,2),'go');
 % hold on;
 % 
-% plot(f_u(:,1),f_u(:,2),'go');
+% plot(f_u(:,1),f_u(:,2),'bo');
 % hold on;
-% 
-% x = 0:1500;
-% y = 0:1500;
-% [X,Y] = meshgrid(x,y);
-% 
-% xx = [X(:) Y(:)];
-% p = mvnpdf(xx,u_u,sigma_u);
-% p = reshape(p,[length(y),length(x)]);
-% contour(X,Y,p);
 % 
 % axis equal;
 % 
@@ -218,4 +71,213 @@ end
 % 
 % legend('a','o','u');
 
+%% calcular u_k inicial para cada clase
 
+u = zeros(K,2);
+
+for k=1:K
+    u(k,:) = mean(f{k},1);
+end
+
+%% calcular sigma_k para cada clase y después hacer el promedio para sigma total
+
+sigma_k = cell(1,K);
+
+for k=1:K
+    sigma_k{k} = calcular_sigma(f{k},u(k,:));
+end
+
+% calculo la probabilidad de cada clase como # muestras total clase / # muestras total
+
+p = zeros(1,K);
+
+total = 0;
+
+for k=1:K
+    n_k = length(f{k});
+    total = total + n_k;
+    p(k) = n_k;
+end
+
+p = p*1/total;
+
+sigma = zeros(2,2);
+
+for k=1:K
+    sigma = sigma + p(k)*sigma_k{k};
+end
+
+
+%% test
+
+xs = [];
+
+% pongo todas las muestras juntas
+
+for k = 1:K
+    xs = [xs;t{k}];
+end
+
+% ws: clase REAL 1..K a la que pertenece cada muestra
+
+ws = [];
+
+for k=1:K
+    ws = [ws , ones(1,length(t{k}))*k];
+end
+
+% c: clase PREDICHA 1..K para cada muestra
+
+c = zeros(1,length(xs));
+
+det_sigma = det(sigma);
+inv_sigma = inv(sigma);
+
+for i = 1: length(xs)
+    x = xs(i,:);
+    
+    % p_x(k) = p(k|x)
+    
+    p_x = zeros(1,K);
+    
+    for k=1:K
+        p_x(k) = g_k(x(1),x(2),det_sigma, inv_sigma,u(k,:),p(k));
+    end
+    
+    [m,k_max] = max(p_x);
+    
+    c(i) = k_max;
+end
+
+%% grafico
+
+figure;
+
+% clasificación correcta
+graficar_muestras(xs,ws,'x',legends, colors, f1_min, f1_max, f2_min, f2_max, K);
+
+hold on
+
+% resultados de test
+graficar_muestras(xs,c,'o',legends, colors, f1_min, f1_max, f2_min, f2_max, K);
+
+title('Test (o) y correcta (x)');
+
+%% calcular error como #clasificaciones correctas/#total muestras
+fprintf('Error: %0.2f %% \n', sum(ws ~= c)/length(xs)*100);
+
+%% grafico de g_k para cada clase
+
+f1 = 0:5:2000;
+f2 = 0:5:2000;
+[F1,F2] = meshgrid(f1,f2);
+
+z = cell(1,K);
+surf_k = [];
+for k=1:K
+    z{k} = zeros(size(F1));
+    for i=1:numel(F1)
+        x = F1(i);
+        y = F2(i);
+        z{k}(i) = g_k(x,y,det_sigma, inv_sigma,u(k,:), p(k));
+    end
+    
+    surf_k(k) = figure;
+    surf(F1,F2,z{k},'EdgeColor','none');
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+    view(2)
+        xlim([f1_min, f1_max])
+    ylim([f2_min, f2_max])
+    
+    str = sprintf('g_k para k=%i (%s)',k,legends(k));
+    title(str)
+end
+
+% esto es para poner el colormap en la misma escala para todos
+
+zz = cell2mat(z);
+zz = reshape(zz,1,numel(zz));
+min_g_k = min(zz);
+max_g_k = max(zz);
+
+for k=1:K
+    figure(surf_k(k));
+    caxis([min_g_k, max_g_k]);
+    colormap jet
+end
+
+%% grafico de la región correspondiente a cada clase en el plano f1,f2
+
+clas = zeros(size(F1));
+
+for i=1:numel(F1)
+    v = zeros(1,K);
+    
+    for k=1:K
+        v(k) = z{k}(i);
+    end
+    
+    [m,clas(i)] = max(v);
+end
+
+figure;
+h = surf(F1,F2,clas,'EdgeColor','none');
+xlabel('F1 [Hz]');
+ylabel('F2 [Hz]');
+    xlim([f1_min, f1_max])
+    ylim([f2_min, f2_max])
+    
+view(2)
+
+title('Region para cada clase');
+
+hold on
+
+map = reshape(cell2mat(colors),K,3);
+
+colormap(map);
+
+% esto es un truco para que aparezca la leyenda en el surf
+
+plot_k = [];
+for k=1:K
+    plot_k(k) = plot(x_k{k}(1,1),x_k{k}(1,2),'o','color',colors{k}, 'markerfacecolor',colors{k});
+    hold on;
+end
+
+legend(plot_k,legends);
+
+
+%% graficar elipses de varianza
+N = 50;
+t = [0:2*pi/(N-1):2*pi];
+y1 = sin(t);
+y2 = cos(t);
+
+y = [y1;y2];
+
+e = cell(1,K);
+
+x = cell(1,K);
+
+figure;
+
+plot_k = [];
+
+for k=1:K
+    e{k} = chol(sigma_k{k});
+    x{k} = e{k}'*y;
+    for i=1:length(x{k})
+        x{k}(:,i) = x{k}(:,i) + u(k,:)';
+    end
+    plot_k(k) = plot(x{k}(1,:),x{k}(2,:),'color',colors{k});
+    hold on;
+    plot(f{k}(:,1),f{k}(:,2),'o','color',colors{k});
+    hold on;
+end
+
+title('Varianza para cada clase')
+
+legend(plot_k,legends);
